70530b2f306277446e467931a96a333997d98da7
MdHusainThekiya <hussainthekiya@gmail.com>
2023-07-29 16:28:41 +0530

    completed with linier search

---
+++ 278_first_bad_version.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+// BRUIT-FORCE SOLUTION
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function(n) {
+    for (let index = 1; index <= n; index++) {
+      if (isBadVersion(index)) {
+        return index;
+      }
+    }
+  };
+};
