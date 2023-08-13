0317887fc552df357bf83d8632de4620f65905d8
MdHusainThekiya <hussainthekiya@gmail.com>
2023-08-13 21:30:17 +0530

    completed 53. Maximum Subarray

--- 53_maximum_subarray.js
+++ 53_maximum_subarray.js
@@ -3,16 +3,50 @@
  * @return {number}
  */
 
+// var maxSubArray = function (nums) {
+//   let result = nums[0];
+//   for (let startPointer = 0; startPointer < nums.length; startPointer++) {
+//     let thisSum = result;
+//     let tempAdd = 0;
+//     for (let endPointer = startPointer; endPointer < nums.length; endPointer++) {
+//       tempAdd += nums[endPointer];
+//       if (tempAdd >= thisSum) { thisSum = tempAdd };
+//     }
+//     if (thisSum >= result) { result = thisSum };
+//   }
+//   return result
+// };
+
+
 var maxSubArray = function (nums) {
-  let result = nums[0];
-  for (let startPointer = 0; startPointer < nums.length; startPointer++) {
-    let thisSum = result;
-    let tempAdd = 0;
-    for (let endPointer = startPointer; endPointer < nums.length; endPointer++) {
-      tempAdd += nums[endPointer];
-      if (tempAdd >= thisSum) { thisSum = tempAdd };
+
+  let startPointer = 0;
+  let endPointer = 0;
+  let maxYet = 0;
+  let tempVal = 0;
+
+  while (startPointer < nums.length && endPointer < nums.length) {
+    if (nums[endPointer] + tempVal <= 0) {
+      endPointer++;
+      startPointer = endPointer;
+      tempVal = 0;
+    } else {
+      tempVal += nums[endPointer];
+      if (tempVal > maxYet) {
+        maxYet = tempVal;
+      }
+      endPointer++;
     }
-    if (thisSum >= result) { result = thisSum };
   }
-  return result
+
+  if (maxYet === 0) {
+    maxYet = nums[0];
+    nums.forEach((val) => {
+      if (maxYet < val) {
+        maxYet = val;
+      }
+    })
+  }
+
+  return maxYet;
 };
