19888ec6eddde5799691567a0b8da5c1bbfb8c48
MdHusainThekiya <hussainthekiya@gmail.com>
2023-08-19 14:02:47 +0530

    completed 3_longest_substring_without_repeating_characters.js using sets in 0(n), O(n)

--- 3_longest_substring_without_repeating_characters.js
+++ 3_longest_substring_without_repeating_characters.js
@@ -2,24 +2,48 @@
  * @param {string} s
  * @return {number}
  */
+
+/** bruitforce method */
+// var lengthOfLongestSubstring = function (s) {
+//   let lastLongestLength = 0;
+//   let longestSubString = '';
+//   let mainIndex = 0;
+
+//   for (let index = 0; index < s.length; index++) {
+//     let thisVal = s[index];
+
+//     if (!longestSubString.includes(thisVal)) {
+//       longestSubString+=thisVal;
+//     } else {
+//       longestSubString = '';
+//       index = mainIndex;
+//       mainIndex++;
+//     }
+//     if (lastLongestLength < longestSubString.length) {
+//       lastLongestLength = longestSubString.length;
+//     }
+//   }
+//   return lastLongestLength;
+// };
+
+/** using sets */
 var lengthOfLongestSubstring = function (s) {
-  let lastLongestLength = 0;
-  let longestSubString = '';
-  let mainIndex = 0;
+  if (s.length < 2) return s.length;
 
-  for (let index = 0; index < s.length; index++) {
-    let thisVal = s[index];
+  let set = new Set();
+  let maxSize = 0;
+  let leftPointer = 0;
+  let rightPointer = 0;
 
-    if (!longestSubString.includes(thisVal)) {
-      longestSubString+=thisVal;
-    } else {
-      longestSubString = '';
-      index = mainIndex;
-      mainIndex++;
+  while(rightPointer < s.length) {
+    while (set.has(s[rightPointer])) { // [a, b, c, b] [lP => 0, rP => 3]
+      set.delete(s[leftPointer]);
+      leftPointer++;
     }
-    if (lastLongestLength < longestSubString.length) {
-      lastLongestLength = longestSubString.length;
-    }
+    set.add(s[rightPointer]);
+    maxSize = Math.max( maxSize, set.size );
+    rightPointer++;
   }
-  return lastLongestLength;
+
+  return maxSize;
 };
