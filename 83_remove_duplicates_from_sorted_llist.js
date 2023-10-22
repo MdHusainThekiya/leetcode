d20441acfe2c3600730dc06f1eb2c218f87d8666
MdHusainThekiya <hussainthekiya@gmail.com>
2023-10-23 00:35:20 +0530

    rewrite and test 83. Remove Duplicates from Sorted List

--- 83_remove_duplicates_from_sorted_llist.js
+++ 83_remove_duplicates_from_sorted_llist.js
@@ -1,22 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
 var deleteDuplicates = function (head) {
-  // Special case...
-  if (head == null || head.next == null)
+
+  if (!head || !head.next) {
     return head;
-  // Initialize a pointer curr with the address of head node...
-  let curr = head;
-  // Traverse all element through a while loop if curr node and the next node of curr node are present...
-  while (curr != null && curr.next != null) {
-    // If the value of curr is equal to the value of prev...
-    // It means the value is present in the linked list...
-    if (curr.val == curr.next.val) {
-      // Hence we do not need to include curr again in the linked list...
-      // So we increment the value of curr...
-      curr.next = curr.next.next;
-    }
-    // Otherwise, we increment the curr pointer...
-    else {
-      curr = curr.next;
+  }
+
+  let tHead = head;
+
+  while (tHead && tHead.next) {
+    if (tHead.val === tHead.next.val) {
+      tHead.next = tHead.next.next
+    } else {
+      tHead = tHead.next
     }
   }
-  return head;        // Return the sorted linked list without any duplicate element...
+
+  return head;
 };
+
+
+const {LinkedList} = require('./index.js');
+
+let linkedList = new LinkedList();
+let array = [1,1,2,3,3];
+for (let index = 0; index < array.length; index++) {
+  linkedList.add(array[index]);
+}
+
+console.log(deleteDuplicates(linkedList));
