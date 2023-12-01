f922ead71d345fce4971fb7c8dfd42bdf46f0af2
Md Husain Thekiya <hussainthekiya@gmail.com>
2023-12-02 00:06:49 +0530

    completed with optimized method

--- 739_daily_temperatures.js
+++ 739_daily_temperatures.js
@@ -1,31 +1,56 @@
+// /**
+//  * @param {number[]} temperatures
+//  * @return {number[]}
+//  */
+// var dailyTemperatures = function(temperatures) {
+
+//   if (temperatures.length <= 1) {
+//     return [0];
+//   }
+
+//   let diffDays = []; 
+//   let firstP = 0;
+//   let secondP = 1;
+
+//   while( firstP <= temperatures.length - 1) {
+    
+//     if (temperatures[secondP] > temperatures[firstP]) {
+//       diffDays.push((secondP - firstP));
+//     } else if ( secondP >= temperatures.length - 1 ) {
+//       diffDays.push(0);
+//     } else {
+//       secondP++;
+//       continue;
+//     }
+//     firstP = firstP + 1;
+//     secondP = firstP + 1;
+
+//   }
+
+//   return diffDays;
+// };
+
+
+
+/** OPTIMIZED METHOD */
 /**
  * @param {number[]} temperatures
  * @return {number[]}
  */
-var dailyTemperatures = function(temperatures) {
+var dailyTemperatures = function (temperatures) {
 
-  if (temperatures.length <= 1) {
-    return [0];
-  }
+  let res = new Array(temperatures.length).fill(0);
+  let monotonicStack = []; // [index, temp]
 
-  let diffDays = []; 
-  let firstP = 0;
-  let secondP = 1;
-
-  while( firstP <= temperatures.length - 1) {
-    
-    if (temperatures[secondP] > temperatures[firstP]) {
-      diffDays.push((secondP - firstP));
-    } else if ( secondP >= temperatures.length - 1 ) {
-      diffDays.push(0);
-    } else {
-      secondP++;
-      continue;
+  for (let i = 0; i < temperatures.length; i++) {
+    while (monotonicStack.length > 0
+      && temperatures[i] > monotonicStack[monotonicStack.length - 1][1]
+    ) {
+      let [index, temp] = monotonicStack.pop();
+      res[index] = (i - index)
     }
-    firstP = firstP + 1;
-    secondP = firstP + 1;
-
+    monotonicStack.push([i, temperatures[i]])
   }
-
-  return diffDays;
+  
+  return res;
 };
